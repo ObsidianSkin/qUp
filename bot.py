ac347a643b78002f0cc1e804408a6d7edbe5d981
Blake Childress <parentalface@gmail.com>
2019-11-02 04:50:53 -0500

    Tracks rections to msg. More to be done...

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 """
--print !help 1 time
 -get guild bot is in
 -add functionality to qUp command
+    -count down by checking which players launched the game (count up when someone closes the game?)
+        -track by reacts instead??
+    -timeout after watiting for players for so long
+-error checking (game not found?)
+-bots own channel? only handles 1 queue at a time??
 """
 # bot.py
 import os
 
-#import discord
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -16,7 +20,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 #client = discord.Client()
 bot = commands.Bot(command_prefix='!')
-"""
+
+msg_id = 0
+
 @bot.event
 async def on_ready():
     #guild = discord.utils.get(bot.guilds, name=GUILD)
@@ -24,12 +30,31 @@ async def on_ready():
         f'{bot.user.name} is connected to the following guild:\n'
         #f'{guild.name}(id: {guild.id})'
     )
-"""
+
 @bot.command(name='qUp', help='Finds friends who are looking to play games!')
 async def queue_up(ctx, game, number_of_players):
-    response = f'{ctx.author} wants {number_of_players} people to play {game}!'
-    await ctx.send(response)
+    global msg_id
+    text = f'{ctx.author} wants {number_of_players} people to play {game}!'
+    msg = await ctx.send(text)
+    await msg.add_reaction('💯')
+    msg_id = msg.id
+    #print(msg.id)
 
+@bot.event
+async def on_raw_reaction_add(payload):
+    message_id = payload.message_id
+    #print(f'{message_id}\n{msg_id}')
+    # and payload.user_id != bot.user_id
+    #print(payload.user_id)
+    if message_id == msg_id and payload.user_id != 640022874631176193:
+        print('wow')
+
+@queue_up.error
+async def queue_up_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Usage: !qUp <game> <number of players>')
+
+#keep this at the bottom
 bot.run(TOKEN)
 """
 @client.event
